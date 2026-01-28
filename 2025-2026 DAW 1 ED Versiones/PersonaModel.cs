bcef980f26efad344248b93232e171f070ec01de
RicardoDigitech <ricardo.sornosa@digitechfp.com>
2026-01-28 12:32:04 +0100

    Clase PersonaModel creada.

---
+++ 2025-2026 DAW 1 ED Versiones/PersonaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_2026_DAW_1_ED_Versiones
+{
+    public class PersonaModel
+    {
+
+    }
+}
